077f5853ad4d5967791eb432159e6b0dd342a759
Anang FIrmansyah <anangfirmansyahp5@gmail.com>
2024-01-05 12:39:02 +0800

    fix: remove middleware

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const rewrites = async () => [
+  {
+    source: "/docs",
+    destination: "https://mindseat.mintlify.app/introduction",
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  rewrites,
+  assetPrefix: "https://mindseat.mintlify.app"
 }
 
 module.exports = nextConfig
