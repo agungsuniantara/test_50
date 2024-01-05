47fdeee47bbf235c64f818e29e551e43bd424fb3
Anang FIrmansyah <anangfirmansyahp5@gmail.com>
2024-01-05 12:47:02 +0800

    vercel

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-const rewrites = async () => [
-  {
-    source: "/docs",
-    destination: "https://mindseat.mintlify.app/introduction",
-  },
-];
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites,
-  assetPrefix: "https://mindseat.mintlify.app"
+
 }
 
 module.exports = nextConfig
